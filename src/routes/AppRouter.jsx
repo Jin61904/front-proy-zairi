f826a6b6ec18169a3d97207f6a18cc2b72dee514
Mila <galindo.camilaandrea@gmail.com>
2024-11-24 17:40:53 -0500

    Se agrega la ruta de inicio de sesión

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
-import ReviewsPage from '../pages/ReviewPage';
+import ReviewPage from '../pages/ReviewPage';
 import Header from '../components/shared/Header';
+import LoginPage from '../pages/LoginPage';
 
 const AppRouter = () => {
     return (
         <Router>
-            <Header/>
+            <Header /> {/* Componente de encabezado, se renderiza fuera de las rutas */}
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/reviews" element={<ReviewsPage />} />
+                <Route path="/" element={<HomePage />} /> {/* Ruta para la página principal */}
+                <Route path="/reviews" element={<ReviewPage />} /> {/* Ruta para la página de reseñas */}
+                <Route path="/login" element={<LoginPage />} /> {/* Ruta para la página de reseñas */}
             </Routes>
         </Router>
     );
 };
 
 export default AppRouter;
+
